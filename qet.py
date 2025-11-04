71b54d142e44bb89b1f4f8e618fa1f410ad67026
sakeeb-droid <alamsmsakeeb@gmail.com>
2025-11-05 02:43:39 +0600

    added long range qet implementation

--- qet.py
+++ qet.py
@@ -60,8 +60,8 @@ class MinimalQETCircuit:
             bitstring = orig_bitstring[::-1]
             ene_A += self.h*(-1)**int(bitstring[0])*count/self.shots
 
-        for i in range(count):
-            error_A.append(self.h*(-1)**int(bitstring[0]))
+            for i in range(count):
+                error_A.append(self.h*(-1)**int(bitstring[0]))
 
         std_A = np.std(error_A)/np.sqrt(self.shots)
         E_A = self.h**2/np.sqrt(self.h**2+self.k**2)
@@ -81,8 +81,8 @@ class MinimalQETCircuit:
                 bitstring = orig_bitstring[::-1]
                 ene_V += 2*self.k*(-1)**int(bitstring[0])*(-1)**int(bitstring[1])*count/self.shots
 
-            for i in range(count):
-                error_V.append(2*self.k*(-1)**(int(bitstring[1])))
+                for i in range(count):
+                    error_V.append(2*self.k*(-1)**(int(bitstring[1])))
 
             std_V = np.std(error_V)/np.sqrt(self.shots)
             print(f"The interacting energy, E_V: {ene_V} ± {std_V}")
@@ -97,8 +97,8 @@ class MinimalQETCircuit:
                 bitstring = orig_bitstring[::-1]
                 ene_B += self.h*(-1)**int(bitstring[1])*count/self.shots
 
-            for i in range(count):
-                error_B.append(self.h*(-1)**int(bitstring[1]))
+                for i in range(count):
+                    error_B.append(self.h*(-1)**int(bitstring[1]))
 
             std_B = np.std(error_B)/np.sqrt(self.shots)
             print(f"Bob's measured local energy, E_B: {ene_B} ± {std_B}")
@@ -106,3 +106,60 @@ class MinimalQETCircuit:
             return ene_B, std_B
         else:
             raise ValueError("To calculate Bob's local energy, set v_measure=False when initializing the MinimalQETCircuit class.")
+        
+
+class LongRangeQETCircuit(MinimalQETCircuit):
+    def __init__(self, h, k, v_measure=False):
+        self.h = h
+        self.k = k
+        self.v_measure = v_measure
+        self.alpha = -np.arcsin((1/np.sqrt(2))*(np.sqrt(1+h/np.sqrt(h**2+k**2))))
+        self.phi = 0.5*np.arcsin((h*k)/np.sqrt((h**2+2*k**2)**2+(h*k)**2))
+        self.qreg = QuantumRegister(4, 'q')
+        self.creg_qt = ClassicalRegister(2, 'c_qt')
+        self.creg_qet = ClassicalRegister(2, 'c_qet')
+        self.qc = QuantumCircuit(self.qreg, self.creg_qt, self.creg_qet)
+
+    def build_circuit(self):
+        self.qc.ry(2*self.alpha, self.qreg[0])
+        self.qc.cx(self.qreg[0], self.qreg[1])
+        self.qc.h(self.qreg[0])
+
+        self.qc.measure(self.qreg[0], self.creg_qet[0])
+
+        with self.qc.if_test((self.creg_qet[0], 0)):
+            self.qc.ry(2*self.phi, self.qreg[1])
+        with self.qc.if_test((self.creg_qet[1], 1)):
+            self.qc.ry(-2*self.phi, self.qreg[1])
+
+        if self.v_measure:
+            self.qc.h(self.qreg[1])
+
+        self.qc.barrier()
+
+        self.qc.h(self.qreg[2])
+        self.qc.cx(self.qreg[2], self.qreg[3])
+        self.qc.cx(self.qreg[1], self.qreg[2])
+        self.qc.h(self.qreg[1])
+        self.qc.measure(self.qreg[1], self.creg_qt[0])
+        self.qc.measure(self.qreg[2], self.creg_qt[1])
+        with self.qc.if_test((self.creg_qt[0], 1)):
+            self.qc.z(self.qreg[3])
+        with self.qc.if_test((self.creg_qt[1], 1)):
+            self.qc.x(self.qreg[3])
+        self.qc.barrier()
+
+        
+        self.qc.measure(self.qreg[3], self.creg_qet[1])
+
+    def get_counts(self, backend, shots=1024, optimization_level=1):
+        self.backend = backend
+        self.shots = shots
+        self.optimization_level = optimization_level
+
+        sampler = Sampler(mode=self.backend)
+        pm = generate_preset_pass_manager(self.optimization_level, self.backend)
+        job = sampler.run(pm.run([self.qc]), shots=self.shots)
+        counts = job.result()
+
+        return counts[0].data.c_qet.get_counts()
